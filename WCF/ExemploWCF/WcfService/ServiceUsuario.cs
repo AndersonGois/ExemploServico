9a9cc7c08e3c91988cd175eac0a344bf5bdffe52
Anderson <andersonrejane@hotmail.com>
2013-12-26 12:13:57 -0200

    Camada de implementacao do servico

---
+++ WCF/ExemploWCF/WcfService/ServiceUsuario.cs
@@ -0,0 +1,24 @@
+﻿
+using WcfService.DataContract.DataContract;
+using WcfService.DataContract.MessageDataContract;
+using WcfService.DataContract.ServiceDataContract;
+
+namespace WcfService
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        public UsuarioResponse GetUsuario(UsuarioRequest usuarioRequest)
+        {
+            var usuarioResponse = new UsuarioResponse
+                {
+                    UsuarioDataContract =
+                        new UsuarioDataContract
+                            {
+                                Codigo = usuarioRequest.UsuarioDataContract.Codigo,
+                                Nome = string.Concat(usuarioRequest.UsuarioDataContract.Nome, " Servico")
+                            }
+                };
+            return usuarioResponse;
+        }
+    }
+}
