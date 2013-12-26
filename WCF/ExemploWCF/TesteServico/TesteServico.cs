15db5382c46a4f934a9ed4abbba5f8e852471c84
Anderson <andersonrejane@hotmail.com>
2013-12-26 12:40:20 -0200

    Camada de Teste

---
+++ WCF/ExemploWCF/TesteServico/TesteServico.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+using TesteServico.ServiceReference55;
+
+namespace TesteServico
+{
+    [TestFixture]
+    public class TesteServicos
+    {
+        [Test]
+        public void GetUsuario()
+        {
+            using (var usuarioClient = new ServiceUsuarioClient())
+            {
+                var usuarioData = new UsuarioData() { Codigo = 1, Nome = "Anderson" };
+                usuarioClient.GetUsuario(ref usuarioData);
+                Assert.AreEqual(usuarioData.Nome,"Anderson Servico");
+            }
+        }
+    }
+}
